45fed729d9bd62f88186a81b78ae819157c476d1
Mason Posner <masonfromohio@gmail.com>
2020-05-19 20:29:11 -0400

    Changed password to thingy

--- First git linked script.R
+++ First git linked script.R
@@ -4,4 +4,4 @@ print("And now it lives on GitHub")
 
 print("Let's try one more line")
 
-print("OK, now does it remember my keychain password")
+print("OK, now does it remember my keychain thingy")
